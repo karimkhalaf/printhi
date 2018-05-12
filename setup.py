5057754fa74194de91e01b15df1a23afb4580aa5
karimkhalaf <karim.a.khalaf>
2018-05-13 00:17:42 +0200

    no message

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='printhi				',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='karim@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['printhi'],
+     )
